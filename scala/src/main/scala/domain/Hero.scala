99e8f1fec0380a28304fad27b9366063003b03fb
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-13 18:07:40 -0300

    Inventario agregado a heroe

--- scala/src/main/scala/domain/Hero.scala
+++ scala/src/main/scala/domain/Hero.scala
@@ -2,7 +2,7 @@ package domain
 
 import domain.Stat.*
 
-case class Hero(baseAttributes: StatBlock = StatBlock(), job: Option[Job] = None) {
+case class Hero(baseAttributes: StatBlock = StatBlock(), job: Option[Job] = None, inventory: Inventory = Inventory()) {
     private def stats(): StatBlock = job.map(_.modifiers + baseAttributes).getOrElse(baseAttributes)
 
     def stat(which: Stat): Int =
