7a90c3ada41f3fcf6c93dfe80a52fbfb9766edf0
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-13 02:06:31 -0300

    Ejemplos de trabajos implementados

--- scala/src/main/scala/domain/Job.scala
+++ scala/src/main/scala/domain/Job.scala
@@ -1,3 +1,11 @@
 package domain
 
+import domain.Stat.{Intelligence, Speed, Strength}
+
 case class Job(modifiers: StatBlock, mainStat: Stat)
+
+object Guerrero extends Job(StatBlock(health = 10, strength = 15, intelligence = -10), Strength)
+
+object Mago extends Job(StatBlock(strength = -20, intelligence = 20), Intelligence)
+
+object Ladron extends Job(StatBlock(health = -5, speed = 10), Speed)
