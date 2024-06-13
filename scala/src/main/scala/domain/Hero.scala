650ceac2100cdb89e9ce3a22c727c9540acc34cf
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-13 02:11:23 -0300

    Heroe tiene los stats afectados por su trabajo

--- scala/src/main/scala/domain/Hero.scala
+++ scala/src/main/scala/domain/Hero.scala
@@ -1,11 +1,16 @@
 package domain
 
+import domain.Stat.*
+
 case class Hero(baseAttributes: StatBlock = StatBlock(), job: Option[Job] = None) {
-    def health(): Int = baseAttributes.health.max(1)
+    private def stats(): StatBlock = job.map(_.modifiers + baseAttributes).getOrElse(baseAttributes)
 
-    def strength(): Int = baseAttributes.strength.max(1)
-
-    def speed(): Int = baseAttributes.speed.max(1)
-
-    def intelligence(): Int = baseAttributes.intelligence.max(1)
+    def stat(which: Stat): Int =
+        1.max(
+            which match
+                case Health => stats().health
+                case Strength => stats().strength
+                case Speed => stats().speed
+                case Intelligence => stats().intelligence
+        )
 }
--- scala/src/test/scala/domain/HeroTest.scala
+++ scala/src/test/scala/domain/HeroTest.scala
@@ -1,14 +1,24 @@
 package domain
 
+import domain.Stat.*
 import org.scalatest.flatspec.AnyFlatSpec
 
 class HeroTest extends AnyFlatSpec {
     "Un heroe" should "redondear stats no positivos a 1" in {
-        val unHeroe: Hero = Hero(StatBlock(2, 1, 0, -1))
+        val hero: Hero = Hero(StatBlock(health = 2, strength = 1, intelligence = -1))
 
-        assert(unHeroe.health() === 2)
-        assert(unHeroe.strength() === 1)
-        assert(unHeroe.speed() === 1)
-        assert(unHeroe.intelligence() === 1)
+        assert(hero.stat(Health) === 2)
+        assert(hero.stat(Strength) === 1)
+        assert(hero.stat(Speed) === 1)
+        assert(hero.stat(Intelligence) === 1)
+    }
+
+    "Un heroe empleado" should "tener sus stats modificados" in {
+        val hero: Hero = Hero(StatBlock(health = 1, strength = 2, speed = 3, intelligence = 4), Some(Guerrero))
+
+        assert(hero.stat(Health) === 11)
+        assert(hero.stat(Strength) === 17)
+        assert(hero.stat(Speed) === 3)
+        assert(hero.stat(Intelligence) === 1)
     }
 }
