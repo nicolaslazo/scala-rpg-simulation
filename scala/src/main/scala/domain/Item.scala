dbe524890a1c736d69a2f85e7f71f0f80e373977
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-14 17:02:52 -0300

    Par de items agregados

--- scala/src/main/scala/domain/Item.scala
+++ scala/src/main/scala/domain/Item.scala
@@ -1,5 +1,6 @@
 package domain
 
+import cats.syntax.option.*
 import domain.ItemSlot.*
 import domain.Stat.*
 
@@ -7,7 +8,7 @@ import scala.collection.immutable.HashMap
 
 case class Item(modifiers: StatBlock = HashMap(),
                 equipCondition: Option[Hero => Boolean] = None,
-                effect: Option[Hero => StatBlock] = None,
+                effect: Option[(StatBlock, Hero) => StatBlock] = None,
                 slot: ItemSlot)
 
 object CascoVikingo extends Item(
@@ -26,4 +27,30 @@ object PalitoMagico extends Item(
 
 object ArcoViejo extends Item(modifiers = HashMap(Strength -> 2), slot = BothHands)
 
-// object TalismanDeDedicacion extends Item(effect = Some(_.stats))
+object TalismanDeDedicacion extends Item(
+    effect = Some(
+        (currentStats, hero) => {
+            // TODO: Hay alguna manera más idiomática de escribir esto?
+            val bonus: Int = hero.job.map(_.mainStat).flatMap(currentStats.get).map(_ * .1).map(_.toInt).getOrElse(0)
+
+            currentStats.map((stat, value) => (stat, value + bonus))
+        }
+    ),
+    slot = Neck)
+
+// TODO: Probablemente poco legible?
+object TalismanDelMinimalismo extends Item(
+    modifiers = HashMap(Health -> 60), // +10 que lo mencionado en consigna para considerar el talisman en sí
+    effect = Some((currentStats, hero) =>
+        currentStats.updatedWith(Health)(value => Some(value.getOrElse(0).-(10 * hero.equipped.size)))),
+    slot = Neck)
+
+object VinchaDelBufaloDeAgua extends Item(
+    equipCondition = Some(_.job.isEmpty),
+    effect = Some((currentStats, _) =>
+        if (for (str <- currentStats.get(Strength); int <- currentStats.get(Intelligence)) yield str > int)
+            .getOrElse(false)
+        then currentStats.updatedWith(Intelligence)(_.getOrElse(0).+(30).some)
+        else currentStats.map((k, v) => if k == Intelligence then (k, v) else (k, v + 10))),
+    slot = Head
+)
