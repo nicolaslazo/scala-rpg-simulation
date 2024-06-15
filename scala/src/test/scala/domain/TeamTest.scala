f51c89a7442b35140bca5b077bec89a5098d49dc
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-14 21:08:55 -0300

    Tipado

--- scala/src/test/scala/domain/TeamTest.scala
+++ scala/src/test/scala/domain/TeamTest.scala
@@ -9,7 +9,7 @@ class TeamTest extends AnyFlatSpec {
     "Un equipo" should "usar un criterio para seleccionar el mejor miembro" in {
         val goodHero: Hero = Hero(HashMap(Speed -> 2))
 
-        assert(Team(name = "", members = List(goodHero, Hero())).bestMember(_.stat(Speed)).get == goodHero)
+        assert(Team(name = "", members = Set(goodHero, Hero())).bestMember(_.stat(Speed)).get == goodHero)
     }
 
     "Un equipo" should "decir que no tiene un mejor miembro si está vacío" in {
