17a2c3073622c2f599cb40d5a438193cec750e09
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-17 10:24:56 -0300

    Equipos terminados

--- scala/src/main/scala/domain/Team.scala
+++ scala/src/main/scala/domain/Team.scala
@@ -1,12 +1,41 @@
 package domain
 
+import cats.syntax.option.*
 import domain.equipment.ItemSlot.{LeftHand, RightHand, SingleHand}
 import domain.equipment.{EquipProjection, Item, ItemSlot}
 
 import scala.collection.immutable.HashMap
+import scala.util.{Failure, Success, Try}
 
 case class Team(name: String, members: Set[Hero] = Set(), earnings: Int = 0) {
+    lazy val leader: Option[Hero] = {
+        val maxMainStat = members.map(_.mainStatPoints).maxOption
+        val leaderCandidates = maxMainStat.map(maxPoints => members.filter(_.mainStatPoints == maxPoints))
+
+        if leaderCandidates.getOrElse(Set()).size == 1 then leaderCandidates.get.last.some else None
+    }
     val bestMember: (Hero => Int) => Option[Hero] = members.maxByOption
+
+    def addItem(item: Item): Team = {
+        val equipProjections: HashMap[Hero, EquipProjection] = memberEquipProjections(
+            membersThatCanEquip(item),
+            item,
+            item.slot)
+        val bestCandidate: Option[EquipProjection] =
+            equipProjections.values.filter(_.pointsDelta > 0).maxByOption(_.pointsDelta)
+
+        bestCandidate match {
+            case Some(projection) => this.copy(members = members - projection.preEquipHero + projection.postEquipHero)
+            case None => this.copy(earnings = earnings + item.value)
+        }
+    }
+
+    def addMember(hero: Hero): Team = this.copy(members = members + hero)
+
+    def replaceMember(oldMember: Hero, newMember: Hero): Try[Team] =
+        if members.contains(oldMember)
+        then Success(this.copy(members = members - oldMember + newMember))
+        else Failure(Exception("El heroe a remover no se encuentra en este equipo"))
 
     private def membersThatCanEquip(item: Item): Set[Hero] =
         item.equipCondition.map(condition => members.filter(condition)).getOrElse(members)
@@ -26,19 +55,5 @@ case class Team(name: String, members: Set[Hero] = Set(), earnings: Int = 0) {
 
         // TODO: Entender notación _*
         HashMap(people.map(hero => hero -> hero.withItemEquippedProjection(item, targetSlot).get).toSeq: _*)
-    }
-
-    def addItem(item: Item): Team = {
-        val equipProjections: HashMap[Hero, EquipProjection] = memberEquipProjections(
-            membersThatCanEquip(item),
-            item,
-            item.slot)
-        val bestCandidate: Option[EquipProjection] =
-            equipProjections.values.filter(_.pointsDelta > 0).maxByOption(_.pointsDelta)
-
-        bestCandidate match {
-            case Some(projection) => this.copy(members = members - projection.preEquipHero + projection.postEquipHero)
-            case None => this.copy(earnings = earnings + item.value)
-        }
     }
 }
--- scala/src/test/scala/domain/TeamTest.scala
+++ scala/src/test/scala/domain/TeamTest.scala
@@ -7,6 +7,7 @@ import domain.equipment.{CascoVikingo, Item}
 import org.scalatest.flatspec.AnyFlatSpec
 
 val mage: Hero = Hero(job = Mago.some)
+val warrior: Hero = Hero(job = Guerrero.some)
 
 class TeamTest extends AnyFlatSpec {
     "Un equipo" should "usar un criterio para seleccionar el mejor miembro" in {
@@ -48,11 +49,43 @@ class TeamTest extends AnyFlatSpec {
 
     "Un equipo con un item nuevo" should "dárselo a la persona que más se beneficie de el" in {
         val item = Item(modifiers = StatBlock(Strength -> 10, Intelligence -> 20), slot = Head)
-        val team = Team("", Set(mage, Hero(job = Guerrero.some)))
+        val team = Team("", Set(mage, warrior))
 
         val teamWithItem = team.addItem(item)
 
         assert(teamWithItem.members.find(_.job == Mago.some).get.equippedItems.contains(item))
         assert(!teamWithItem.members.find(_.job == Guerrero.some).get.equippedItems.contains(item))
+    }
+
+    "Un equipo" should "poder agregar miembros" in {
+        assert(Team("").addMember(mage).members.size == 1)
+    }
+
+    "Un equipo" should "poder reemplazar un miembro con otro" in {
+        val teamWithReplacement = Team("", members = Set(mage)).replaceMember(mage, warrior)
+
+        assert(teamWithReplacement.get.members == Set(warrior))
+    }
+
+    "Un equipo" should "fallar en reemplazar un miembro que no está en el" in {
+        assert(Team("").replaceMember(mage, mage).isFailure)
+    }
+
+    "Un equipo" should "poder discernir su líder si este existe" in {
+        val badMage = Hero(StatBlock(Intelligence -> 10), job = Mago.some) // Inteligencia = 10 + 20 = 30
+        val goodWarrior = Hero(StatBlock(Strength -> 20), job = Guerrero.some) // Fuerza = 20 + 15 = 35
+
+        assert(Team("", members = Set(badMage, goodWarrior)).leader == goodWarrior.some)
+    }
+
+    "Un equipo" should "no poder declarar su líder si el resultado es ambiguo" in {
+        val someMage = Hero(StatBlock(Intelligence -> 10), job = Mago.some) // Inteligencia 10 + 20 = 30
+        val someWarrior = Hero(StatBlock(Strength -> 15), job = Guerrero.some) // Fuerza 15 + 15 = 30
+
+        assert(Team("", members = Set(someMage, someWarrior)).leader.isEmpty)
+    }
+
+    "Un equipo vacío" should "no tener un líder" in {
+        assert(Team("").leader.isEmpty)
     }
 }
