f9f39508e2ee15d2b07ddd9988e51743f2b72a78
nicolaslazo <45973144+nicolaslazo@users.noreply.github.com>
2024-06-19 23:47:09 -0300

    StatBlock ya no puede aplicar modificadores que le dejen atributos base negativos

--- scala/src/main/scala/domain/StatBlock.scala
+++ scala/src/main/scala/domain/StatBlock.scala
@@ -18,7 +18,7 @@ extension (statBlock: StatBlock) {
     def getStat(stat: Stat): Int = statBlock.getOrElse(stat, 0)
 
     def applyModifiers(modifiers: StatBlock): StatBlock =
-        statBlock.merged(modifiers) { case ((k, v1), (_, v2)) => (k, v1 + v2) }
+        statBlock.merged(modifiers) { case ((k, v1), (_, v2)) => (k, (v1 + v2).max(0)) }
 
     def applyEffect(effect: (StatBlock, Hero) => StatBlock, context: Hero): StatBlock = effect(statBlock, context)
 
